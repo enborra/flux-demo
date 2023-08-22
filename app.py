9bde66da2143d96394923e3b3162f31c615f52fc
Andres Krogh-Walker <andres@Silvermini.local>
2023-08-22 13:01:03 -0700

    adding scaffolding for static file delivery

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import send_from_directory
+
 
 app = Flask(__name__)
 
@@ -6,3 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return 'tada.'
+
+@app.route('/static/<path:path>')
+def send_static(path):
+	return send_from_directory('static', path)
