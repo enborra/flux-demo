0552a0c4338b3296dec255041b450ceeaa6091c0
Andres Krogh-Walker <andres@Silvermini.local>
2023-08-22 15:47:08 -0700

    dummy plugin from GoMakeThings

--- static/demo-one.js
+++ static/demo-one.js
@@ -1,30 +1,29 @@
-console.log('demo script loaded!');
-
-
-
 (function (root, factory) {
 	if ( typeof define === 'function' && define.amd ) {
 		define(['buoy'], factory(root));
 	} else if ( typeof exports === 'object' ) {
 		module.exports = factory(require('buoy'));
 	} else {
-		root.clickMe = factory(root, root.buoy);
+		root.myPlugin = factory(root, root.buoy);
 	}
-})(typeof global !== 'undefined' ? global : this.window || this.global, function (root) {
+})(typeof global !== "undefined" ? global : this.window || this.global, function (root) {
 
 	'use strict';
+
+	console.log('demo script loaded!');
 
 	//
 	// Variables
 	//
 
-	var clickMe = {}; // Object for public APIs
+	var myPlugin = {}; // Object for public APIs
 	var supports = !!document.querySelector && !!root.addEventListener; // Feature test
 	var settings; // Placeholder variables
 
 	// Default settings
 	var defaults = {
-		resizeLog: 'The window was resized!',
+		someVar: 123,
+		initClass: 'js-myplugin',
 		callbackBefore: function () {},
 		callbackAfter: function () {}
 	};
@@ -34,73 +33,32 @@ console.log('demo script loaded!');
 	// Methods
 	//
 
-	/**
-	 * Add a class to a link when it's clicked
-	 * @private
-	 * @param {Event} event The click event
-	 */
-	var addClass = function ( event ) {
-
-		// Get the thing that was clicked
-		var toggle = event.target;
-
-		// Check if the thing that was clicked has the [data-click-me] attribute
-		if ( toggle && toggle.hasAttribute( 'data-click-me' ) ) {
-
-			// Prevent default click event
-			if ( toggle.tagName.toLowerCase() === 'a') {
-				event.preventDefault();
-			}
-
-			// Set the [data-click-me] value as a class on the link
-			toggle.classList.add( toggle.getAttribute( 'data-click-me' ) );
-
-		}
-
-	};
+	// @todo add plugin methods here
 
 	/**
 	 * Handle events
 	 * @private
 	 */
 	var eventHandler = function (event) {
-
-		// Callback before the event handler runs
-		settings.callbackBefore;
-
-		// On click
-		if ( event.type === 'click' ) {
-			addClass( event );
-		}
-
-		// On resize
-		if ( event.type === 'resize' ) {
-			console.log( settings.resizeLog );
-		}
-
-		// Callback after the event handler runs
-		settings.callbackAfter;
-
+		// @todo Do something on event
 	};
 
 	/**
 	 * Destroy the current initialization.
 	 * @public
 	 */
-	clickMe.destroy = function () {
+	myPlugin.destroy = function () {
 
 		// If plugin isn't already initialized, stop
 		if ( !settings ) return;
 
-		// Remove all added classes
-		var links = document.querySelectorAll( '[data-click-me]' );
-		for ( var i = 0, len = links.length; i < len; i++  ) {
-			links[i].classList.remove( links[i].getAttribute( 'data-click-me' ) );
-		}
+		// Remove init class for conditional CSS
+		document.documentElement.classList.remove( settings.initClass );
+
+		// @todo Undo any other init functions...
 
 		// Remove event listeners
 		document.removeEventListener('click', eventHandler, false);
-		window.removeEventListener('resize', eventHandler, false);
 
 		// Reset variables
 		settings = null;
@@ -112,22 +70,26 @@ console.log('demo script loaded!');
 	 * @public
 	 * @param {Object} options User settings
 	 */
-	clickMe.init = function ( options ) {
+	myPlugin.init = function ( options ) {
 
-		console.log( 'demo plugin init!' );
+		console.log('demo script init!');
 
 		// feature test
 		if ( !supports ) return;
 
 		// Destroy any existing initializations
-		clickMe.destroy();
+		myPlugin.destroy();
 
 		// Merge user options with defaults
 		settings = buoy.extend( defaults, options || {} );
 
+		// Add class to HTML element to activate conditional CSS
+		document.documentElement.classList.add( settings.initClass );
+
+		// @todo Do stuff...
+
 		// Listen for click events
 		document.addEventListener('click', eventHandler, false);
-		window.addEventListener('resize', eventHandler, false);
 
 	};
 
@@ -136,6 +98,6 @@ console.log('demo script loaded!');
 	// Public APIs
 	//
 
-	return clickMe;
+	return myPlugin;
 
 });
