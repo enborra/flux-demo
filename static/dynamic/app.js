01fbc06132a99b147244c78826b01429f6a5e78e
Andres Krogh-Walker <andres@Silvermini.local>
2023-08-24 16:09:18 -0700

    adding test widget

--- static/dynamic/app.js
+++ static/dynamic/app.js
@@ -1,8 +1,8 @@
 const Widget = Object.create({
     create(chatId) {
         const wdg = document.createElement("div");
-        wdg.classList.add("chat-box");
-        wdg.innerHTML = `<h1>Chat: ${ chatId }</h1>`;
+        wdg.classList.add("demo-box");
+        wdg.innerHTML = `<h2>Dynamic demo widget: ${ chatId }</h2>`;
         // Load your chat data into UI
         return wdg;
     }
