a71d3ff28fb5efee75d7dfc87c6175ff745c1d8f
Andres Krogh-Walker <andres@Silvermini.local>
2023-08-24 16:24:24 -0700

    adding test widget

--- static/dynamic/app.js
+++ static/dynamic/app.js
@@ -9,8 +9,28 @@ const Widget = Object.create({
     }
 });
 
-const myWidgetInstance = Widget.create("chat-123456");
+// const myWidgetInstance = Widget.create("chat-123456");
 
-const id = `chat-${ Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1) }`;
-document.write(`<div id="${ id }"></div>`);
-document.getElementById(id).appendChild(myWidgetInstance);
+// const id = `chat-${ Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1) }`;
+// document.write(`<div id="${ id }"></div>`);
+// document.getElementById(id).appendChild(myWidgetInstance);
+
+
+
+
+const initWhenReady = () => {
+    removeEventListener("DOMContentLoaded", initWhenReady);
+    
+    // Array.prototype.forEach.call(document.querySelectorAll(".demo-box"), ele => {
+    //     const myWidgetInstance = Widget.create(ele.dataset.chatid);
+    //     ele.appendChild(myWidgetInstance);
+    // });
+
+    const myWidgetInstance = Widget.create("chat-123456");
+
+    const id = `chat-${ Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1) }`;
+    document.write(`<div id="${ id }"></div>`);
+    document.getElementById(id).appendChild(myWidgetInstance);
+};
+
+addEventListener('DOMContentLoaded', initWhenReady);
