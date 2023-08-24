953bb83a749b10e5308fbc1429389474119b5789
Andres Krogh-Walker <andres@Silvermini.local>
2023-08-24 16:31:23 -0700

    adding test widget

--- static/dynamic/app.js
+++ static/dynamic/app.js
@@ -26,10 +26,10 @@ const initWhenReady = () => {
     //     ele.appendChild(myWidgetInstance);
     // });
 
-    const myWidgetInstance = Widget.create("chat-123456");
+    const myWidgetInstance = Widget.create("demo-123456");
 
-    const id = `chat-${ Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1) }`;
-    document.write(`<div id="${ id }"></div>`);
+    const id = `demo-box-dynamic`;
+    // document.write(`<div id="${ id }"></div>`);
     document.getElementById(id).appendChild(myWidgetInstance);
 };
 
